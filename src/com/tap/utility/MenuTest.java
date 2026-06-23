5c113abaf885aed55b8467fd6af108a679f73875
Swapna <swapnanimbaragi@gmail.com>
2026-06-23 22:39:52 +0530

    Added Menu

---
+++ src/com/tap/utility/MenuTest.java
@@ -0,0 +1,81 @@
+package com.tap.utility;
+
+import java.util.List;
+
+import com.tap.DAOimpl.MenuDAOImpl;
+import com.tap.model.Menu;
+
+public class MenuTest {
+
+	public static void main(String[] args) {
+
+		MenuDAOImpl menuDAOImpl = new MenuDAOImpl();
+
+
+//		ADD MENU ITEM
+//		Menu menu = new Menu(
+//				1,
+//				"Chicken Biryani",
+//				"Spicy Hyderabadi Chicken Biryani",
+//				299.00,
+//				"Biryani",
+//				"https://example.com/biryani.jpg",
+//				true,
+//				false,
+//				4.7,
+//				true,
+//				"Medium",
+//				"Full",
+//				10.0,
+//				true
+//		);
+//
+//		menuDAOImpl.addMenu(menu);
+//		System.out.println("Menu Added Successfully");
+
+
+
+//		GET ONE MENU ITEM
+//		Menu menu = menuDAOImpl.getMenu(1);
+//		System.out.println(menu);
+
+
+
+//		UPDATE MENU ITEM
+//		Menu menu = menuDAOImpl.getMenu(1);
+//
+//		menu.setPrice(349.00);
+//		menu.setRating(4.9);
+//		menu.setDiscountPercentage(15.0);
+//		menu.setBestSeller(true);
+//
+//		menuDAOImpl.updateMenu(menu);
+//
+//		System.out.println("Menu Updated Successfully");
+
+
+
+//		DELETE MENU ITEM
+//		menuDAOImpl.deleteMenu(1);
+//
+//		System.out.println("Menu Deleted Successfully");
+
+
+
+//		GET ALL MENU ITEMS
+//		List<Menu> menus = menuDAOImpl.getAllMenu();
+//
+//		for(Menu menu : menus) {
+//			System.out.println(menu);
+//		}
+
+
+
+//		GET MENU BY RESTAURANT ID
+		List<Menu> menus = menuDAOImpl.getMenuByRestaurant(1);
+
+		for(Menu menu : menus) {
+			System.out.println(menu);
+		}
+	}
+}
